92c2133abd6e9724ff7935d29b5801cb5e4abf7b
Mr-bikash <122956511+Mr-bikash@users.noreply.github.com>
2023-01-22 11:58:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+'''
+USING THE STATICFG LIBRARY TO DRAW THE CONTROL FLOW GRAPH
+Stored in png format
+'''
+
 from staticfg import CFGBuilder
 
-cfg = CFGBuilder().build_from_file('test','test.py')
-cfg.build_visual('test','png')
+cfg = CFGBuilder().build_from_file('test','test.py')  #builds the diagraph, stored as test
+cfg.build_visual('test','png')                        #builds the visual representation of the diagraph, stored as test.png
